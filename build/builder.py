91d67dd9fd9e051598681f6594eff1386dfcae5a
huang yuxin <huangyuxin@isrc.iscas.ac.cn>
2023-08-07 08:22:53 +0000

    update build/builder.py.
    Signed-off-by: huang yuxin <huangyuxin@isrc.iscas.ac.cn>

--- build/builder.py
+++ build/builder.py
@@ -24,6 +24,7 @@ from builder.commands.format import Formatter
 from builder.commands.package import Packager
 from builder.common.utils import exec_sys_command
 from builder.common.env_checker import checker
+from builder.common.prebuild import get_machine_info
 from builder.common.logger import logger, LoggerManager
 
 VERSION="0.1.0-rc1"
@@ -177,8 +178,6 @@ class FtBuilder:
         if (subcommand == "build"):
             builder = Builder(self.args)
             rst = builder.exec_command()
-            if rst is True: 
-                exec_sys_command(['sudo', 'cp', os.path.join(builder.build_output_dir, 'common/common/*.so'), "/usr/lib64/"])
         elif (subcommand == "format"):
             formatter = Formatter(self.args)
             rst = formatter.exec_command()
@@ -207,7 +206,20 @@ def main() -> int:
     try:
         builder.prepare()
 
-        return 0 if builder.do_subcommand() else 1
+        if builder.do_subcommand() is False:
+            return 1
+        else:
+            arch = builder.args.target_cpu
+            if arch == "auto":
+                arch, _ = get_machine_info()
+            build_output_dir = os.path.join(builder.args.project_dir, "out", builder.args.build_type.title(), arch)
+
+            librarys = os.listdir(os.path.join(build_output_dir, 'common/common/'))
+            for lib in librarys:
+                if '.so' in lib:
+                    exec_sys_command(['sudo', 'cp', os.path.join(build_output_dir, 'common/common/', lib), "/usr/lib64/"])[0]
+            return 0
+
     except:
         console.print_exception(show_locals=True)
         return 1
